fe72527d28b147c9b5720f524e49c7a51c0c87c2
JosealdoLyra <josealdo.lyra@gmail.com>
2025-12-02 11:31:34 -0300

    ATUALIZAÇÃO

--- database/rasp_pn.sql
+++ database/rasp_pn.sql
@@ -6,8 +6,11 @@
 create table rasp_pn (
     id_rasp_pn serial primary key,
 
-    -- Relacionamento com a tabela principal
+    -- Relacionamento com a tabela principal RASP
     id_rasp int not null references rasp(id_rasp),
+
+    -- Relacionamento com a classificação aplicada ao SPPS/PT para este PN
+    id_spps_classificacao_rasp int references spps_classificacao_rasp(id_spps_classificacao_rasp),
 
     -- Peça envolvida
     pn varchar(30) not null,
@@ -17,13 +20,14 @@ create table rasp_pn (
     quantidade_checada int,
     quantidade_rejeitada int,
 
-    -- Flag de contenção
+    -- Flag de contenção (controle operacional)
     em_contencao boolean default false,
 
-    -- Informações de fornecedor
+    -- Informações do fornecedor (opcional)
     duns varchar(15),
 
     -- Ordem de exibição no front-end
     ordem_exibicao smallint not null
 );
+
 
